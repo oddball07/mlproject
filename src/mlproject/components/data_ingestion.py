528d85e8f602d84d8ee44df4b089eb8ab71fd157
Hiten Tharani <hiten.tharani@gmail.com>
2026-02-26 18:05:52 +0530

    second commit

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
+from src.mlproject.utils import read_sql_data
 
 from dataclasses import dataclass
 
@@ -18,9 +19,13 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
         try:
+            df = read_sql_data()
             logging.info("Reading from SQL Database")
+
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             
+            
+
         except Exception as e:
             raise CustomException(e, sys)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,6 +4,8 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+import pymysql
+
 
 load_dotenv()
 
@@ -13,3 +15,12 @@ password = os.getenv("PASSWORD")
 db_name = os.getenv("DB")
 
 def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb = pymysql.connect(host=host, user=user, password=password, database=db_name)
+        logging.info("SQL database connection established: %s", mydb)
+        df = pd.read_sql_query('Select * from students', mydb)
+        print (df.head())
+        return df
+    except Exception as e:
+        raise CustomException(e, sys)
