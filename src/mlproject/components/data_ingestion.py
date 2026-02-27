6c3e53ac9fd09917377cb930212f1f2d447c724d
Hiten Tharani <hiten.tharani@gmail.com>
2026-02-27 16:04:34 +0530

    Data Ingestion

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.mlproject.logger import logging
 import pandas as pd
 from src.mlproject.utils import read_sql_data
 
+from sklearn.model_selection import train_test_split
+
 from dataclasses import dataclass
 
 @dataclass
@@ -25,7 +27,17 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             
-            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            df.to_csv(self.ingestion_config.train_data_path, index=False)
+            df.to_csv(self.ingestion_config.test_data_path, index=False)
+
+            logging.info("Data ingestion is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
 
         except Exception as e:
             raise CustomException(e, sys)
